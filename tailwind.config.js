fab2e642d2d27a99d6658e42129a70827c45ebd0
Zaiidmo <zaiidmoumnii@gmail.com>
2024-03-02 23:59:11 +0100

    Environment Setup : Configuring TailwindCss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ content: [
 "./resources/**/*.js",
 "./resources/**/*.vue",
 ], theme: {
-extend: {},
+extend: {
+  colors: {
+    "primary": "#E50914",
+    "secondary": "#FFD166",
+    "component":"#404040",
+    "subtle": "#CCCCCC"
+  },
+  fontFamily: {
+    trade: ["Trade Winds", "system-ui"],
+    yellowTTail: ["Yellow Tail", "cursive"],
+    poppins: ["Poppins", "sans-serif"],
+    supermercado: ["Supermercado One", "sans-serif"],
+    buttons: ["Seaweed Script", "cursive"],
+
+
+  },
+},
 },
 plugins: [],
 }
