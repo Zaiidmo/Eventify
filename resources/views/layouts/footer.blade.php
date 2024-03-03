c8159216904a71fd06782201852a7bf13e359cd2
Zaiidmo <zaiidmoumnii@gmail.com>
2024-03-03 14:37:52 +0100

    FrontEnd : Footer Not Completed Yet

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,115 @@
+<footer class="w-full border-t-4 border-primary">
+    <div class="max-w-screen-xl mx-auto flex justify-between">
+        <div class="w-full flex items-center justify-between p-6">
+            <a href="#" class="text-2xl font-bold font-salsa text-white">
+                <img src="{{ asset('Logo.png') }}" alt="Eventify Logo" class="w-40">
+            </a>
+            <div class="w-full lg:w-6/12 px-4">
+                <div class="flex flex-wrap items-top">
+                    <div class="w-full lg:w-4/12 px-4 ml-auto">
+                        <ul class="list-unstyled">
+                            <li>
+                                <a class="text-subtle hover:text-component font-semibold block pb-2 text-sm"
+                                    href="https://www.creative-tim.com/presentation?ref=njs-profile">Home</a>
+                            </li>
+                            <li>
+                                <a class="text-subtle hover:text-component font-semibold block pb-2 text-sm"
+                                    href="https://blog.creative-tim.com?ref=njs-profile">About Us</a>
+                            </li>
+                            <li>
+                                <a class="text-subtle hover:text-component font-semibold block pb-2 text-sm"
+                                    href="https://blog.creative-tim.com?ref=njs-profile">Contact Us</a>
+                            </li>
+                            <li>
+                                <a class="text-subtle hover:text-component font-semibold block pb-2 text-sm"
+                                    href="https://www.github.com/creativetimofficial?ref=njs-profile">Up Coming Events</a>
+                            </li>
+                            <li>
+                                <a class="text-subtle hover:text-component font-semibold block pb-2 text-sm"
+                                    href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Categories</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="w-full lg:w-8/12 px-4">
+                        <ul class="list-unstyled">
+                            <li>
+                                <div class="flex justify-between">
+                                    <div class="flex gap-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="#ffffff" stroke-linecap="round" stroke-width="2"><rect width="18" height="14" x="3" y="5" stroke-dasharray="64" stroke-dashoffset="64" rx="1"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="64;0"/></rect><path stroke-dasharray="24" stroke-dashoffset="24" d="M3 6.5L12 12L21 6.5"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.6s" dur="0.4s" values="24;0"/></path></g></svg>
+                                        <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
+                                            Email 
+                                        </p>
+                                    </div>
+                                    <div class="flex gap-4">
+                                        <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
+                                            Support@eventify.co.uk 
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex justify-between">
+                                    <div class="flex gap-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="#ffffff" stroke-linecap="round" stroke-width="2"><rect width="18" height="14" x="3" y="5" stroke-dasharray="64" stroke-dashoffset="64" rx="1"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="64;0"/></rect><path stroke-dasharray="24" stroke-dashoffset="24" d="M3 6.5L12 12L21 6.5"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.6s" dur="0.4s" values="24;0"/></path></g></svg>
+                                        <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
+                                            Email 
+                                        </p>
+                                    </div>
+                                    <div class="flex gap-4">
+                                        <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
+                                            Support@eventify.co.uk 
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex justify-between">
+                                    <div class="flex gap-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="#ffffff" stroke-linecap="round" stroke-width="2"><rect width="18" height="14" x="3" y="5" stroke-dasharray="64" stroke-dashoffset="64" rx="1"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="64;0"/></rect><path stroke-dasharray="24" stroke-dashoffset="24" d="M3 6.5L12 12L21 6.5"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.6s" dur="0.4s" values="24;0"/></path></g></svg>
+                                        <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
+                                            Email 
+                                        </p>
+                                    </div>
+                                    <div class="flex gap-4">
+                                        <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
+                                            Support@eventify.co.uk 
+                                        </p>
+                                    </div>
+                                </div>
+                            <li>
+                                <div class="flex justify-between">
+                                    <div class="flex gap-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="#ffffff" stroke-linecap="round" stroke-width="2"><rect width="18" height="14" x="3" y="5" stroke-dasharray="64" stroke-dashoffset="64" rx="1"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="64;0"/></rect><path stroke-dasharray="24" stroke-dashoffset="24" d="M3 6.5L12 12L21 6.5"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.6s" dur="0.4s" values="24;0"/></path></g></svg>
+                                        <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
+                                            Email 
+                                        </p>
+                                    </div>
+                                    <div class="flex gap-4">
+                                        <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
+                                            Support@eventify.co.uk 
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex justify-between">
+                                    <div class="flex gap-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="#ffffff" stroke-linecap="round" stroke-width="2"><rect width="18" height="14" x="3" y="5" stroke-dasharray="64" stroke-dashoffset="64" rx="1"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="64;0"/></rect><path stroke-dasharray="24" stroke-dashoffset="24" d="M3 6.5L12 12L21 6.5"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.6s" dur="0.4s" values="24;0"/></path></g></svg>
+                                        <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
+                                            Email 
+                                        </p>
+                                    </div>
+                                    <div class="flex gap-4">
+                                        <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
+                                            Support@eventify.co.uk 
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
