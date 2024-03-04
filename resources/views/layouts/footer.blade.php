70c60459c65b5fc7217e0c0998c9a1ae1f505c01
Zaiidmo <zaiidmoumnii@gmail.com>
2024-03-04 11:27:59 +0100

    FrontEnd : Editin Footer Icons

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -36,21 +36,7 @@
                             <li>
                                 <div class="flex justify-between">
                                     <div class="flex gap-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24">
-                                            <g fill="none" stroke="#ffffff" stroke-linecap="round" stroke-width="2">
-                                                <rect width="18" height="14" x="3" y="5" stroke-dasharray="64"
-                                                    stroke-dashoffset="64" rx="1">
-                                                    <animate fill="freeze" attributeName="stroke-dashoffset"
-                                                        dur="0.6s" values="64;0" />
-                                                </rect>
-                                                <path stroke-dasharray="24" stroke-dashoffset="24"
-                                                    d="M3 6.5L12 12L21 6.5">
-                                                    <animate fill="freeze" attributeName="stroke-dashoffset"
-                                                        begin="0.6s" dur="0.4s" values="24;0" />
-                                                </path>
-                                            </g>
-                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#ffffff" d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2zm-2 0l-8 5l-8-5zm0 12H4V8l8 5l8-5z"/></svg>
                                         <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
                                             Email
                                         </p>
@@ -65,49 +51,7 @@
                             <li>
                                 <div class="flex justify-between">
                                     <div class="flex gap-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24">
-                                            <g fill="none" stroke="#ffffff" stroke-linecap="round"
-                                                stroke-linejoin="round" stroke-width="2">
-                                                <path stroke-dasharray="64" stroke-dashoffset="64"
-                                                    d="M8 3C8.5 3 10.5 7.5 10.5 8C10.5 9 9 10 8.5 11C8 12 9 13 10 14C10.3943 14.3943 12 16 13 15.5C14 15 15 13.5 16 13.5C16.5 13.5 21 15.5 21 16C21 18 19.5 19.5 18 20C16.5 20.5 15.5 20.5 13.5 20C11.5 19.5 10 19 7.5 16.5C5 14 4.5 12.5 4 10.5C3.5 8.5 3.5 7.5 4 6C4.5 4.5 6 3 8 3Z">
-                                                    <animate fill="freeze" attributeName="stroke-dashoffset"
-                                                        dur="0.6s" values="64;0" />
-                                                    <animateTransform attributeName="transform"
-                                                        begin="0.6s;lineMdPhoneCallLoop0.begin+2.6s" dur="0.5s"
-                                                        type="rotate"
-                                                        values="0 12 12;15 12 12;0 12 12;-12 12 12;0 12 12;12 12 12;0 12 12;-15 12 12;0 12 12" />
-                                                </path>
-                                                <path stroke-dasharray="4" stroke-dashoffset="4"
-                                                    d="M14 7.04404C14.6608 7.34734 15.2571 7.76718 15.7624 8.27723M16.956 10C16.6606 9.35636 16.2546 8.77401 15.7624 8.27723"
-                                                    opacity="0">
-                                                    <set id="lineMdPhoneCallLoop0" attributeName="opacity"
-                                                        begin="0.7s;lineMdPhoneCallLoop0.begin+2.7s" to="1" />
-                                                    <animate fill="freeze" attributeName="stroke-dashoffset"
-                                                        begin="0.7s;lineMdPhoneCallLoop0.begin+2.7s" dur="0.2s"
-                                                        values="4;8" />
-                                                    <animate fill="freeze" attributeName="stroke-dashoffset"
-                                                        begin="1.3s;lineMdPhoneCallLoop0.begin+3.3s" dur="0.3s"
-                                                        values="0;4" />
-                                                    <set attributeName="opacity"
-                                                        begin="1.6s;lineMdPhoneCallLoop0.begin+3.6s" to="0" />
-                                                </path>
-                                                <path stroke-dasharray="10" stroke-dashoffset="10"
-                                                    d="M20.748 9C20.3874 7.59926 19.6571 6.347 18.6672 5.3535M15 3.25203C16.4105 3.61507 17.6704 4.3531 18.6672 5.3535"
-                                                    opacity="0">
-                                                    <set attributeName="opacity"
-                                                        begin="1s;lineMdPhoneCallLoop0.begin+3s" to="1" />
-                                                    <animate fill="freeze" attributeName="stroke-dashoffset"
-                                                        begin="1s;lineMdPhoneCallLoop0.begin+3s" dur="0.2s"
-                                                        values="10;20" />
-                                                    <animate fill="freeze" attributeName="stroke-dashoffset"
-                                                        begin="1.5s;lineMdPhoneCallLoop0.begin+3.5s" dur="0.3s"
-                                                        values="0;10" />
-                                                    <set attributeName="opacity"
-                                                        begin="1.8s;lineMdPhoneCallLoop0.begin+3.8s" to="0" />
-                                                </path>
-                                            </g>
-                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#ffffff" d="m19.23 15.26l-2.54-.29a1.99 1.99 0 0 0-1.64.57l-1.84 1.84a15.045 15.045 0 0 1-6.59-6.59l1.85-1.85c.43-.43.64-1.03.57-1.64l-.29-2.52a2.001 2.001 0 0 0-1.99-1.77H5.03c-1.13 0-2.07.94-2 2.07c.53 8.54 7.36 15.36 15.89 15.89c1.13.07 2.07-.87 2.07-2v-1.73c.01-1.01-.75-1.86-1.76-1.98"/></svg>
                                         <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
 
                                             Phone
@@ -137,22 +81,7 @@
                             <li>
                                 <div class="flex justify-between">
                                     <div class="flex gap-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24">
-                                            <g fill="none" stroke="#ffffff" stroke-linecap="round"
-                                                stroke-width="2">
-                                                <rect width="18" height="14" x="3" y="5" stroke-dasharray="64"
-                                                    stroke-dashoffset="64" rx="1">
-                                                    <animate fill="freeze" attributeName="stroke-dashoffset"
-                                                        dur="0.6s" values="64;0" />
-                                                </rect>
-                                                <path stroke-dasharray="24" stroke-dashoffset="24"
-                                                    d="M3 6.5L12 12L21 6.5">
-                                                    <animate fill="freeze" attributeName="stroke-dashoffset"
-                                                        begin="0.6s" dur="0.4s" values="24;0" />
-                                                </path>
-                                            </g>
-                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#ffffff" d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4s4-1.79 4-4s-1.79-4-4-4m8.94 3A8.994 8.994 0 0 0 13 3.06V2c0-.55-.45-1-1-1s-1 .45-1 1v1.06A8.994 8.994 0 0 0 3.06 11H2c-.55 0-1 .45-1 1s.45 1 1 1h1.06A8.994 8.994 0 0 0 11 20.94V22c0 .55.45 1 1 1s1-.45 1-1v-1.06A8.994 8.994 0 0 0 20.94 13H22c.55 0 1-.45 1-1s-.45-1-1-1zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7s7 3.13 7 7s-3.13 7-7 7"/></svg>
                                         <p class="text-subtle hover:text-component font-semibold block pb-2 text-sm">
                                             Address
                                         </p>
